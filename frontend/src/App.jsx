bffd3991acc57ee4b7666b1a67233d06d56a1b4c
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-21 00:27:46 +0100

    basic app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+
+const App = () => {
+  return (
+   <h1>Hello this frontend does work </h1>
+  );
+};
+
+export default App;
