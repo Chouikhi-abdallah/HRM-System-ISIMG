6756a7fb24274302effff3344edbce6209312263
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-21 00:28:45 +0100

    created manager page

---
+++ frontend/src/pages/Manager.jsx
@@ -0,0 +1,10 @@
+function Manager() {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <h1 className="text-2xl">Welcome, Manager!</h1>
+      </div>
+    );
+  }
+  
+  export default Manager;
+  
