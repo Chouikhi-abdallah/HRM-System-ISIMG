ed11be829cf2cd00109f9acff72256847e6411f0
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-21 00:28:19 +0100

    created admin page

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+function Admin() {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <h1 className="text-2xl">Welcome, Admin!</h1>
+      </div>
+    );
+  }
+  
+  export default Admin;
+  
