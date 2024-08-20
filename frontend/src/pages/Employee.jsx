63efc5d5605fae9f8bf23074781e7ef631ace3d1
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-21 00:28:31 +0100

    created employee page

---
+++ frontend/src/pages/Employee.jsx
@@ -0,0 +1,10 @@
+function Employee() {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <h1 className="text-2xl">Welcome, Developer!</h1>
+      </div>
+    );
+  }
+  
+  export default Employee;
+  
