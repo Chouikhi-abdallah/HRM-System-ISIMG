ab319617965250f36be0cb28a4b19e5d9f2d535b
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-21 00:23:31 +0100

    mounting the departement, employee,payroll, and tasks routes

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const authRoutes = require('./routes/authRoutes');
 const managerRouter= require('./routes/managerRoute');
-
 const employeeRouter= require('./routes/employeeRoutes');
-
+const taskRouter= require('./routes/taskRoutes');
+const payrollRouter= require('./routes/payrollRoutes');
+const departementRouter= require('./routes/departementsRoute');
+const cors=require('cors');
 const app = express();
 const prisma = new PrismaClient();
 
@@ -12,6 +14,8 @@ const prisma = new PrismaClient();
 // Parse incoming JSON content of requests or response
 // and make them available in req.body as an object
 app.use(express.json()); 
+
+app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -24,6 +28,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/employees', employeeRouter);
 // add manager routes to /api/managers
 app.use('/api/managers', managerRouter);
+// add task routes to /api/tasks
+app.use('/api/tasks', taskRouter);
+//add payroll routes to /api/payrolls
+app.use('/api/payroll',payrollRouter);
+//add department routes to /api/departments
+app.use('/api/departments',departementRouter );
 
 // add a route to handle 404 errors
 app.use('*', (req, res) => { 
