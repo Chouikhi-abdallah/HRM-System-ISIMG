0253258549922e2041b7e7145f3691eaf58d86aa
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-25 01:40:25 +0100

    fix: added the route for getting all employees by departement id

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getAllEmployees, getEmployeeById}= require('../controllers/employeeController');
+const {getAllEmployees, getEmployeeById,getAllEmployeesByDepartement}= require('../controllers/employeeController');
 
 // Routes
 router.get('/all', getAllEmployees);
 router.get('/byid/:id', getEmployeeById);
+router.get('/bydepartment/:departmentId', getAllEmployeesByDepartement);
 
 module.exports = router;
