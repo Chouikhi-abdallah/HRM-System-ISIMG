62b474b557379fed62c469061cdccf9cbf3a5dca
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-30 03:08:20 +0100

    fix:removed reactStrictmodefor the drag and drop compnenets  and added react toasts and added a page for the random routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
@@ -7,9 +6,14 @@ import Signup from './pages/Signup';
 import Admin from './pages/Admin';
 import Manager from './pages/Manager';
 import Employee from './pages/Employee';
+import Error from './pages/error';
+import 'react-toastify/dist/ReactToastify.css';
+import ForgetPassword from './components/ForgetPassword';
+import ResetPassword from './pages/ResetPassword';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
@@ -17,7 +21,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/admin" element={<Admin />} />
         <Route path="/manager" element={<Manager />} />
         <Route path="/employee" element={<Employee />} />
+        <Route path="*" element={<Error/>}/>
+        <Route path='/'element={<Login/>}/>
+        <Route path='/forgetpassword' element={<ForgetPassword/>}/>
+        <Route path="/resetpassword/:token" element={<ResetPassword />}/>
       </Routes>
     </Router>
-  </React.StrictMode>,
 );
