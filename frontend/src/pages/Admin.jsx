3f4bd771fe30a2b7233ee913b4d04e770fa7f6df
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-30 03:19:30 +0100

    simple admin Hr frontend just for test

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,10 +1,31 @@
+import  { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+
 function Admin() {
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <h1 className="text-2xl">Welcome, Admin!</h1>
-      </div>
-    );
-  }
-  
-  export default Admin;
-  
+  const [activeContent, setActiveContent] = useState('Dashboard');
+  const userRole = 'HRADMIN';
+
+  const renderContent = () => {
+    switch (activeContent) {
+      case 'Dashboard':
+        return <h1>HR Admin Dashboard with Overview</h1>;
+      case 'Manage Payrolls':
+        return <h1>Manage Payrolls Content</h1>;
+      case 'Manage Leaves':
+        return <h1>Manage Leaves Content</h1>;
+      case 'Manage Trainings':
+        return <h1>Manage Trainings Content</h1>;
+      default:
+        return <h1>HR Admin Dashboard with Overview</h1>;
+    }
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar userRole={userRole} setActiveContent={setActiveContent} />
+      <div className="flex-1 p-6">{renderContent()}</div>
+    </div>
+  );
+}
+
+export default Admin;
