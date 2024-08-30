cd5a5ee164a3163b9d9c214006e331c259695651
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2024-08-30 03:11:31 +0100

    added toastcontainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
-   <h1>Hello this frontend does work </h1>
+    <>
+    <ToastContainer />
+
+   <h1>Hello this frontend does work </h1></>
   );
 };
 
