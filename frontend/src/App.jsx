45d5d08a18a3564a5e52a0d1bded1132b48469c2
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2025-02-03 11:56:15 +0100

    adding toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
   return (
     <>
-    <ToastContainer />
 
    <h1>Hello this frontend does work </h1></>
   );
