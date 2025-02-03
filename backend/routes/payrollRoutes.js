cf0ee763c327bdd5e843aeae3458f6a931423aff
Chouikhi-abdallah <chouikhiabdallahpro@gmail.com>
2025-02-03 11:54:54 +0100

    updated some endpoints for the payrolls

--- backend/routes/payrollRoutes.js
+++ backend/routes/payrollRoutes.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
-const {createPayroll,getPayrollByEmployeeId,getPayrollById,updatePayroll,deletePayroll}= require('../controllers/payrollcontroller');
+const {getAllPayrolls,getPayrollByDepartment,createPayroll,getPayrollByVisitorId,getPayrollById,updatePayroll,deletePayroll}= require('../controllers/payrollcontroller');
 
+router.get('/all',getAllPayrolls);
+router.get('/allByDepartement',getPayrollByDepartment);
 router.post('/create',createPayroll);
-router.get('/payrollByEmployeeId/:employeeId',getPayrollByEmployeeId);
+router.get('/payrollByVisitorId/:visitorId', getPayrollByVisitorId); 
 router.get('/payrollById/:payrollId',getPayrollById);
 router.put('/update/:payrollId',updatePayroll);
 router.delete('/delete/:payrollId',deletePayroll);
